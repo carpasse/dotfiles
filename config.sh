17dfc8f04609b6ac1739e9545359690df4efbee6
Carlos Pastor <carlos.pastor@mo-hsk-m-tec066.anl.dmgt.net>
2020-06-02 13:37:16 +0100

    update-config

--- config.sh
+++ config.sh
@@ -1,7 +1,14 @@
 # Main configuration
 
 # Node version management installation dir
+<<<<<<< HEAD
 export NVM_DIR="/Users/carlos.pastor/.nvm"
 
 # Root folder of your projects
 export MOL_DIR="/Users/carlos.pastor/gh/mailOnline/"
+=======
+export NVM_DIR="~/.nvm"
+
+# Root folder of your projects
+export MOL_DIR="~/dev/mailOnline/mol-fe"
+>>>>>>> parent of ed73cb9... feat: set full path on config.sh
