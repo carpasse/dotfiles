965de3103113f6cc0a6cf55e6f3807e95a6aa6df
Carlos Serrano <carpasse@gmail.com>
2020-06-02 13:46:32 +0100

    update config

--- config.sh
+++ config.sh
@@ -1,14 +1,13 @@
 # Main configuration
 
 # Node version management installation dir
-<<<<<<< HEAD
 export NVM_DIR="/Users/carlos.pastor/.nvm"
 
-# Root folder of your projects
-export MOL_DIR="/Users/carlos.pastor/gh/mailOnline/"
-=======
-export NVM_DIR="~/.nvm"
+# Git config
+git config --global user.name "Carlos Serrano"
+git config --global user.email "carpasse@gmail.com"
 
-# Root folder of your projects
-export MOL_DIR="~/dev/mailOnline/mol-fe"
->>>>>>> parent of ed73cb9... feat: set full path on config.sh
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
--- src/git.sh
+++ src/git.sh
@@ -49,8 +49,3 @@ gitpr () {
     open -a /Applications/Google\ Chrome.app "$(find_github_url)/compare/next-release...$branch?expand=1";
   fi
 }
-
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
